1331093cda28b4fcbb44db92175f3545a6af80b7
Andrew Morris <voltrevo@gmail.com>
2025-03-19 14:14:30 +1100

    Add apps/2pc-is-for-lovers, apps/hello

--- scripts/build_extras.sh
+++ scripts/build_extras.sh
@@ -8,16 +8,29 @@ mkdir -p build/apps
 rm -rf build/extras-tmp
 mkdir -p build/extras-tmp
 
-pushd build/extras-tmp
-    curl -L -o repo.zip https://github.com/voltrevo/mpc-lizard-spock/archive/refs/heads/main.zip
-    unzip repo.zip
-    rm repo.zip
+build_app() {
+    local repo_name="$1"
+    local deploy_path="$2"
+    local subdir="$3"
+    local env_var_name="$4"
+    
+    pushd build/extras-tmp
+        curl -L -o repo.zip "https://github.com/voltrevo/${repo_name}/archive/refs/heads/main.zip"
+        unzip repo.zip
+        rm repo.zip
 
-    pushd mpc-lizard-spock-main
-        npm install
-        VITE_MPC_LIZARD_SPOCK_BASE="/apps/lizard-spock/" npm run build
+        pushd "${repo_name}-main/${subdir}"
+            npm install
+            declare "${env_var_name}=/${deploy_path}/"
+            eval "${env_var_name}=/${deploy_path}/ npm run build"
+        popd
     popd
-popd
 
-mv build/extras-tmp/mpc-lizard-spock-main/dist build/apps/lizard-spock
+    mv "build/extras-tmp/${repo_name}-main/${subdir}/dist" "build/${deploy_path}"
+}
+
+build_app "mpc-lizard-spock" "apps/lizard-spock" "." "VITE_MPC_LIZARD_SPOCK_BASE"
+build_app "2pc-is-for-lovers" "apps/2pc-is-for-lovers" "." "VITE_2PC_IS_FOR_LOVERS_BASE"
+build_app "mpc-hello" "apps/hello" "client-client" "VITE_MPC_HELLO_BASE"
+
 rm -rf build/extras-tmp
